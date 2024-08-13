0916f8ea3695c46846d3b33bcd7f0e509beecf4c
Catalina Galan Vargas <catalinagalanvargas@gmail.com>
2024-08-13 20:14:59 +0000

    script psql_docker.sh corrected

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,49 +1,57 @@
 #!/bin/bash
 
-#check if docker-deamon is active
-if [ "$(systemctl is-active docker)" = "active" ]; then
-	echo "$(systemctl status docker)"
-else
-	sudo systemctl start docker
-	echo "$(systemctl status docker)"	
+# start docker
+sudo systemctl status docker || sudo systemctl start docker
 
-fi
+# set CLI arguments
+cmd=$1
+db_username=$2
+db_password=$3
 
-#set arguments
-export db_username=$2
-export db_password=$3
+# container status
+docker container inspect jrvs-psql
+container_status=$?
 
-#check $1 argument for next action
-if [ "$1" != "create" ] && [ "$1" != "start" ] && [ "$1" != "stop" ]; then
-	echo "error: invalid argument."
-        exit 1
-elif [ "$1" = "create" ]; then
-	#check if container already exists
-	if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" = 2 ]; then
-		echo "error: container jrvs-psql already exists."
+case $cmd in
+	create)
+	# check if container exists
+	if [ $container_status -eq 0 ]; then
+		echo "Container already exists"
+		echo
+		echo "$(docker ps -f name=jrvs-psql)"
 		exit 1
-	#check if username and passwords are provided
-	elif [ $# -ne 3 ]; then
-		echo "error: wrong number of arguments. Please provide database username and password"
-		exit 1
-	#create container jrvs-psql
-	else
-		docker volume create pgdata
-		docker run --name jrvs-psql -d -e POSTGRES_USER=$db_username -e POSTGRES_PASSWORD=$db_password  -v pgdata:/var/lib/postgresql/data -p 5433:5432 postgres:9.6-alpine
-		exit $?
 	fi
-#error in case container was not created"
-elif [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" -ne 2 ]; then
-	echo "container jrvs-psql was NOT created successfully."
-	exit 1
-elif [ "$1" = "start" ]; then
-	docker container start jrvs-psql
-	echo "container started"
-	exit $?
-elif [ "$1" = "stop" ]; then
-	docker container stop jrvs-psql
-	echo "container stopped"
-	exit $?
-fi
 
-exit 0
+	# check for correct number or arguments
+	if [ "$#" -ne 3 ]; then
+		echo "Create requires username and password"
+		exit 1
+	fi
+	
+	# create container [db_username] [db_password]
+	docker volume create pgdata
+	docker run --name jrvs-psql -e POSTGRESS_USER=$db_username -e POSTGRES_PASSWORD=$db_password -v pgdata/var/lib/postgresql/data -p 5432:5432 -d postgres:9.6-alpine
+	echo 
+	echo "$(docker ps -f name=jrvs-psql)"
+	exit $?
+	;;
+	
+	start|stop)
+	# check instance status, exit if it hasn't been created
+        if [ $container_status -eq 1 ]; then
+	     	echo "Container doesn't exist"
+        	exit 1
+        fi
+
+	# start|stop the container
+	docker container $cmd jrvs-psql
+	exit $?
+	;;
+
+	*)
+	#check incorrect arguments
+	echo "Illegal command"
+	echo "Commands start|stop|create"
+	exit 1
+	;;
+esac
