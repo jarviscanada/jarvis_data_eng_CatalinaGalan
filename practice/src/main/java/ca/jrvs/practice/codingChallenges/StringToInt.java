28bcb1229fe05299664ba7601b5baee232ec8082
Catalina Galan <catalinagalanvargas@gmail.com>
2024-11-20 15:46:23 -0500

    myAtoiNoParse

--- practice/src/main/java/ca/jrvs/practice/codingChallenges/StringToInt.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenges/StringToInt.java
@@ -36,6 +36,35 @@ public class StringToInt {
   }
 
   public int myAtoiNoParse(String s) {
-    return 0;
+    long answer = 0;
+    int symbol = 1;
+    int i = 0;
+    s = s.trim();
+
+    if (s.isEmpty()) return 0;
+
+    if (s.charAt(0) == '-') {
+      symbol = -1;
+      i++;
+    }
+    if (s.charAt(0) == '+') {
+      i++;
+    }
+
+    while (i < s.length()) {
+      if (Character.isDigit(s.charAt(i))) {
+        int digit = (int) s.charAt(i) - 48;
+        answer = answer * 10 + digit;
+        if (answer * symbol > Integer.MAX_VALUE) {
+          return Integer.MAX_VALUE;
+        } else if (answer * symbol <= Integer.MIN_VALUE) {
+          return Integer.MIN_VALUE;
+        }
+        i++;
+      } else {
+        break;
+      }
+    }
+    return (int)answer * symbol;
   }
 }
--- practice/src/test/java/ca/jrvs/practice/codingChallenges/StringToIntTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenges/StringToIntTest.java
@@ -78,4 +78,78 @@ class StringToIntTest {
     int actual = test.myAtoiParse(s);
     assertEquals(expected, actual);
   }
+
+  @Test
+  void myAtoiNoParseEmpty() {
+    String s = "";
+    int expected = 0;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseMin() {
+    String s = "-91283472332";
+    int expected = -2147483648;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseMax() {
+    String s = "91283472332";
+    int expected = 2147483647;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseTwoSymbols() {
+    String s = "-+123";
+    int expected = 0;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseAllSpaces() {
+    String s = "   ";
+    int expected = 0;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseNonDigit() {
+    String s = "   uj-+ ";
+    int expected = 0;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseDigitFirst() {
+    String s = " 123-9  l";
+    int expected = 123;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseDigitLast() {
+    String s = " -L   123";
+    int expected = 0;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiNoParseDigits() {
+    String s = "123";
+    int expected = 123;
+    int actual = test.myAtoiNoParse(s);
+    assertEquals(expected, actual);
+  }
+
+
 }
