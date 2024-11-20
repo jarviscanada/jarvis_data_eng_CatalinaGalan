31b585868dc582b0c5971381a3d1829c5472c45c
Catalina Galan <catalinagalanvargas@gmail.com>
2024-11-20 14:06:26 -0500

    tests for myAtoyParse

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenges/StringToInt.java
@@ -0,0 +1,12 @@
+package ca.jrvs.practice.codingChallenges;
+
+public class StringToInt {
+
+  public int myAtoiParse(String s) {
+    return 0;
+  }
+
+  public int myAtoiNoParse(String s) {
+    return 0;
+  }
+}
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenges/StringToIntTest.java
@@ -0,0 +1,81 @@
+package ca.jrvs.practice.codingChallenges;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class StringToIntTest {
+
+  StringToInt test = new StringToInt();
+
+  @Test
+  void myAtoiParseEmpty() {
+    String s = "";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseMin() {
+    String s = "";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseMax() {
+    String s = "";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseTwoSymbols() {
+    String s = "-+123";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseAllSpaces() {
+    String s = "   ";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseNonDigit() {
+    String s = "   uj-+ ";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseDigitFirst() {
+    String s = " 123-9  l";
+    int expected = 123;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseDigitLast() {
+    String s = " -L   123";
+    int expected = 0;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void myAtoiParseDigits() {
+    String s = "123";
+    int expected = 123;
+    int actual = test.myAtoiParse(s);
+    assertEquals(expected, actual);
+  }
+}
