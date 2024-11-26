29965a63b46888c20a40986a80363309e3b033f9
Catalina Galan <catalinagalanvargas@gmail.com>
2024-11-26 12:45:52 -0500

    validSudoku optimized

--- practice/src/main/java/ca/jrvs/practice/codingChallenges/ValidSudoku.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenges/ValidSudoku.java
@@ -1,6 +1,8 @@
 package ca.jrvs.practice.codingChallenges;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ValidSudoku {
@@ -18,18 +20,9 @@ public class ValidSudoku {
      answer = checkSection(buildCol(board, i));
     }
 
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-      answer = checkSection(buildBox(board, i, j));
-//      char[] box = new char[9];
-//        int index = 0;
-//        for (int k = i; k < i + 3; k++) {
-//          for (int l = j; l < j + 3; l++) {
-//            box[index] = board[k][l];
-//            index++;
-//          }
-//        }
-
+    for (int i = 0; i < 9; i += 3) {
+      for (int j = 0; j < 9; j += 3) {
+        answer = checkSection(buildBox(board, i, j));
       }
     }
 
@@ -49,9 +42,8 @@ public class ValidSudoku {
 
   public char[] buildCol(char[][] board, int i) {
     char[] col = new char[9];
-    int index = 0;
     for (int j = 0; j < 9; j++) {
-      col[index] = board[i][j];
+      col[j] = board[i][j];
     }
     return col;
   }
@@ -59,14 +51,12 @@ public class ValidSudoku {
   public char[] buildBox(char[][] board, int i, int j) {
     char[] box = new char[9];
     int index = 0;
-    for (int row = 0; row < i + 3; row ++) {
-      for (int col = 0; col < j + 3; col++) {
-        box[index] = board[row][col];
+    for (int k = i; k < i + 3; k++) {
+      for (int l = j; l < j + 3; l++) {
+        box[index] = board[k][l];
         index++;
       }
     }
     return box;
   }
-
-
 }
