bf86ce8c2d6b898a825beef0ca41eebac2b801d6
Catalina Galan <catalinagalanvargas@gmail.com>
2024-11-28 17:59:26 -0500

    fill with paint completed with edge cases (4 sides check)

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenges/FillWithPaint.java
@@ -0,0 +1,40 @@
+package ca.jrvs.practice.codingChallenges;
+
+public class FillWithPaint {
+
+  public int countSections(char[][] picture) {
+    int result = 0;
+    for (int i = 0; i < picture.length; i++) {
+      for (int j = 0; j < picture[0].length; j++) {
+        if (picture[i][j] != '.') {
+          char[][] nextPicture = checkAdjacentCells(picture, i, j);
+          if (nextPicture != null) { result ++; }
+          System.out.println(result);
+        }
+      }
+    }
+    return result;
+  }
+
+  public char[][] checkAdjacentCells(char[][] picture, int x, int y) {
+    char currentChar = picture[x][y];
+    picture[x][y] = '.';
+
+    if (y > 0 && currentChar == picture[x][y - 1]) {
+      checkAdjacentCells(picture, x, y - 1);
+    }
+
+    if (x > 0 && currentChar == picture[x - 1][y]) {
+      checkAdjacentCells(picture, x - 1, y);
+    }
+
+    if (y + 1 < picture[0].length && currentChar == picture[x][y + 1]) {
+      checkAdjacentCells(picture, x, y + 1);
+    }
+    if (x + 1 < picture.length && currentChar == picture[x + 1][y]) {
+      checkAdjacentCells(picture, x + 1, y);
+    }
+
+    return picture;
+  }
+}
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenges/FillWithPaintTest.java
@@ -0,0 +1,41 @@
+package ca.jrvs.practice.codingChallenges;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FillWithPaintTest {
+
+  FillWithPaint fill = new FillWithPaint();
+
+  @Test
+  void countSectionsTest() {
+    char[][] picture = {
+        {'A', 'A', 'A', 'B', 'B', 'C'},
+        {'A', 'A', 'A', 'B', 'C', 'C'},
+        {'A', 'B', 'C', 'C', 'C', 'B'}
+    };
+    assertEquals(5, fill.countSections(picture));
+  }
+
+  @Test
+  void countSectionsTest2() {
+    char[][] picture = {
+        {'A', 'A', 'B', 'B', 'B', 'C'},
+        {'A', 'A', 'A', 'B', 'C', 'C'},
+        {'A', 'B', 'A', 'C', 'C', 'B'}
+    };
+    assertEquals(5, fill.countSections(picture));
+  }
+
+  @Test
+  void countSectionsTest3() {
+    char[][] picture = {
+        {'A', 'A', 'B', 'B', 'B', 'B'},
+        {'A', 'A', 'B', 'A', 'C', 'C'},
+        {'A', 'B', 'A', 'A', 'A', 'C'},
+        {'B', 'B', 'A', 'C', 'C', 'C'}
+    };
+    assertEquals(5, fill.countSections(picture));
+  }
+}
