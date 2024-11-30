8d919c17a3814089c23c4ed46b61d501650a7070
Catalina Galan <catalinagalanvargas@gmail.com>
2024-11-29 21:20:14 -0500

    ValidAnagram leetcode passed for HashMap solution, and StringBuilder solution

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenges/ValidAnagram.java
@@ -0,0 +1,46 @@
+package ca.jrvs.practice.codingChallenges;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ValidAnagram {
+
+  public static boolean validAnagram(String s, String t) {
+
+    if (s.equals(t)) {
+      return true;
+    } else if (s.length() != t.length()) {
+      return false;
+    }
+
+    // using a HashMap
+    Map<Character, Integer> letters = new HashMap<>();
+
+    for (char c : s.toCharArray()) {
+      letters.put(c, letters.getOrDefault(c, 0) + 1);
+    }
+
+    for (char c : t.toCharArray()) {
+      letters.put(c, letters.getOrDefault(c, 0) - 1);
+    }
+
+    Set<Integer> result = new HashSet<>(letters.values());
+    return result.equals(Set.of(0));
+
+    // using StringBuilder
+    // StringBuilder trings = new StringBuilder(t);
+
+    // for (int i = 0; i < s.length(); i++) {
+    //     String check = Character.toString(s.charAt(i));
+    //     if (trings.indexOf(check) >= 0) {
+    //         int j = trings.indexOf(check);
+    //         trings.deleteCharAt(j);
+    //     } else {
+    //         return false;
+    //     }
+    // }
+    // return true;
+  }
+}
