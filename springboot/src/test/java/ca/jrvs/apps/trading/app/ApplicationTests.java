a0a2917bfd43633abbeb62919c8ea32b1ba949cb
Catalina Galan <catalinagalanvargas@gmail.com>
2025-01-07 17:55:02 -0500

    spring boot init from initializer

---
+++ springboot/src/test/java/ca/jrvs/apps/trading/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jrvs.apps.trading.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
