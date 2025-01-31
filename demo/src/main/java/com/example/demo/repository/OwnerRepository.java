fd1b55c082598afc99484a4226e05f057f0701d2
Catalina Galan <catalinagalanvargas@gmail.com>
2025-01-31 15:23:57 -0500

    Succesfully created Cat and Owner using @MapsId using only CatRepository

--- demo/src/main/java/com/example/demo/repository/OwnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRepository extends JpaRepository<Owner, Integer> {
-
-}
