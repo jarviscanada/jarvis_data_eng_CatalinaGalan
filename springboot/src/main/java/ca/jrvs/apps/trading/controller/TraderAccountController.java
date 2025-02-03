b48586806d0541bac2f08f7bd38748c760bdc1ea
Catalina Galan <catalinagalanvargas@gmail.com>
2025-02-03 14:42:27 -0300

    working on user input validation for Trader and Account

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/TraderAccountController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/TraderAccountController.java
@@ -1,12 +1,16 @@
 package ca.jrvs.apps.trading.controller;
 
+import ca.jrvs.apps.trading.model.Account;
 import ca.jrvs.apps.trading.model.Trader;
 import ca.jrvs.apps.trading.service.TraderAccountService;
+import ca.jrvs.apps.trading.util.ResponseExceptionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,6 +21,13 @@ public class TraderAccountController {
 
   @Autowired
   private TraderAccountService traderAccountService;
+
+  @PostMapping("/")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public Trader createTrader(Trader trader) {
+    return traderAccountService.createTraderAndAccount(trader);
+  }
 
   @PostMapping("/firstName/{firstName}/lastName/{lastName}/dob/{dob}/country/{country}/"
       + "email/{email}")
@@ -33,5 +44,35 @@ public class TraderAccountController {
     trader.setEmail(email);
 
     return traderAccountService.createTraderAndAccount(trader);
+  }
+
+  @DeleteMapping("trader/{traderId}")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public void deleteTrader(@PathVariable Long traderId) {
+    traderAccountService.deleteTraderById(traderId);
+    System.out.println("Trader and Account successfully deleted");
+  }
+
+  @PutMapping("/deposit/trader/{traderId}/amount/{amount}")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public Account depositFunds(@PathVariable Long traderId, @PathVariable Double amount) {
+    try {
+      return traderAccountService.deposit(traderId, amount);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
+
+  @PutMapping("/withdraw/trader/{traderID}/amount/{amount}")
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public Account withrowFunds(@PathVariable Long traderId, @PathVariable Double amount) {
+    try {
+      return traderAccountService.withdraw(traderId, amount);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
   }
 }
--- springboot/src/main/java/ca/jrvs/apps/trading/service/TraderAccountService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/TraderAccountService.java
@@ -1,12 +1,15 @@
 package ca.jrvs.apps.trading.service;
 
 import ca.jrvs.apps.trading.model.Account;
+import ca.jrvs.apps.trading.model.Position;
 import ca.jrvs.apps.trading.model.Trader;
 import ca.jrvs.apps.trading.repository.TraderRepository;
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 @Service
 public class TraderAccountService {
@@ -16,12 +19,80 @@ public class TraderAccountService {
 
   public Trader createTraderAndAccount(Trader trader) {
 
-    Account account = new Account();
-    account.setAmount(0.0);
-    account.setTrader(trader);
-    trader.setAccount(account);
-
+    if (trader.getId() != null) {
+      throw new IllegalArgumentException("New trader id must be null");
+    }
+    try {
+      Account account = new Account();
+      account.setAmount(0.0);
+      account.setTrader(trader);
+      trader.setAccount(account);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("All fields for trader must be provided.", e);
+    }
     return traderRepository.save(trader);
   }
 
+  public void deleteTraderById(Long traderId) {
+
+    try {
+      traderRepository.existsById(traderId);
+      Account account = traderRepository.findAccountById(traderId);
+//      List<Position> positions =
+
+      if (account.getAmount() != 0) {
+        throw new IllegalArgumentException("Unable to delete Trader #{traderId}. Account balance must be 0.");
+      }
+      
+//      positions.forEach(position -> () {
+////      securityOrderRepository.deleteById(positionId);
+//      });
+      traderRepository.deleteById(traderId);
+    } catch (Exception e) {
+
+    }
+
+  }
+
+
+  public Account deposit(Long traderId, Double amount) {
+
+    Account account;
+
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Withdraw amount must be greater than 0. Please enter a valid withdraw amount.");
+    }
+
+    try {
+      account = traderRepository.findAccountById(traderId);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("traderId not found. Please provide a valid traderId.", e);
+    }
+
+    double newAmount = account.getAmount() + amount;
+    account.setAmount(newAmount);
+    return account;
+  }
+
+  public Account withdraw(Long traderId, Double amount) {
+
+    Account account;
+
+    try {
+      account = traderRepository.findAccountById(traderId);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("traderId not found. Please provide a valid traderId.", e);
+    }
+
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Withdraw amount must be greater than 0. Please enter a valid withdraw amount.");
+    } else if (amount > account.getAmount()) {
+      throw new IllegalArgumentException("Requested withdraw exceeds available funds.");
+    }
+
+    double newAmount = account.getAmount() - amount;
+    account.setAmount(newAmount);
+    return account;
+  }
 }
+
